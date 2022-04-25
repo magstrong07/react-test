52d53bfd3600f0094c64c18ff4156c9deabf0ac0
Andrey Moskvin <moskowkach@gmail.com>
2022-04-25 23:08:35 +0300

    update p

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           Привет, реакт!
+          Проверка 2!
         </p>
         <a
           className="App-link"
